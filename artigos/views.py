dae625b8b09415d95cc56f44d281606de429dbba
Thiago Rocha <thiagorocha06@gmail.com>
2017-11-30 02:22:12 -0200

    correcao da ordem dos ultimos artigos

--- artigos/views.py
+++ artigos/views.py
@@ -25,7 +25,7 @@ class InicioView(ArtigoMixinDetailView, TemplateView):
     def get_context_data(self, **kwargs):
         context = super(InicioView, self).get_context_data(**kwargs)
         context['artigos_mais_vistos'] = Artigo.objects.all().order_by("-hit_count_generic__hits")[:5]
-        context['ultimos_artigos'] = Artigo.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('data_de_publicacao')[:5]
+        context['ultimos_artigos'] = Artigo.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('-data_de_publicacao')[:5]
         return context
 
 class PostCountHitDetailView(ArtigoMixinDetailView, HitCountDetailView):
