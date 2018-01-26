d6c679e71ba53264158659ee7a323a08a9427dd0
Thiago Rocha <thiagorocha06@gmail.com>
2018-01-25 22:28:41 -0200

    Corrigido erro anterior da localidade

--- exercicios/admin.py
+++ exercicios/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from exercicios.models import Exercicio
 
 class ExercicioModelAdmin(admin.ModelAdmin):
-    list_display = ["especialidade", "ano", "localidade", "prova", "instituicao", "enunciado"]
+    list_display = ["pk","enunciado", "especialidade", "ano", "localidade", "prova", "instituicao"]
     list_filter = ["especialidade", "ano", "localidade"]
     search_fields = ["enunciado"]
     class meta:
