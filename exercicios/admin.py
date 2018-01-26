68b79920c1a32f52afa7cf9a78fe094bcbb641d9
Thiago Rocha <thiagorocha06@gmail.com>
2018-01-25 22:51:33 -0200

    Corrigido erro tema

--- exercicios/admin.py
+++ exercicios/admin.py
@@ -3,7 +3,7 @@ from exercicios.models import Exercicio
 
 class ExercicioModelAdmin(admin.ModelAdmin):
     list_display = ["pk","enunciado", "especialidade","tema", "ano", "localidade"]
-    list_filter = ["especialidade", "ano", "localidade"]
+    list_filter = ["localidade", "especialidade", "tema"]
     search_fields = ["enunciado"]
     class meta:
         model = Exercicio
