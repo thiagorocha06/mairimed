4a81621b305faa86fdf97f6808cd41e17f327e1f
Thiago Rocha <thiagorocha06@gmail.com>
2018-01-28 01:10:50 -0200

    corrigido erro da url gerar simulado

--- exercicios/urls.py
+++ exercicios/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
 
 ### EXERCICIOS ###
     url(r'^exercicios/categorias_lista/$', views.categorias_lista, name='categorias_lista'),
-    url(r'^exercicios/gerar_simulado/$', views.answerpage, name='gerar_simulado'),
     url(r'^exercicios/simulado/$', views.SimuladoExerciciosView.as_view(), name='simulado_exercicios'),
     url(r'^exercicios/cardiologia/$', views.CategoriasExerciciosView.as_view(), name='cardiologia_exercicios'),
     url(r'^exercicios/cirurgia/$', views.CategoriasExerciciosView.as_view(), name='cirurgia_exercicios'),
