16fa117abe4d0f3d616d4f247d393a3072ba59a4
Thiago Rocha <thiagorocha06@gmail.com>
2018-04-05 20:25:37 -0300

    Excluido topicos vazios

--- artigos/forms.py
+++ artigos/forms.py
@@ -8,9 +8,8 @@ class ArtigoForm(forms.ModelForm):
         fields = (
         'modulo', 'categoria',
         'titulo', 'introducao', 'classificacao', 'epidemiologia',
-        'etiologia_fisiopatologia', 'etio_top1', 'etio_texto1', 'etio_top2', 'etio_texto2', 'etio_top3', 'etio_texto3', 'etio_top4', 'etio_texto4',
-        'diagnostico', 'historia_clinica', 'exame_fisico', 'exames_complementares', 'criterios_diagnosticos', 'diagnostico_diferencial',
-        'top1', 'tratamento_e_manejo', 'tratamento_nao_medicamentoso', 'tratamento_medicamentoso',
-        'tratamento_intervencionista',
+        'etiologia_fisiopatologia',
+        'diagnostico', 'exames_complementares', 'criterios_diagnosticos', 'diagnostico_diferencial',
+        'top1', 'tratamento_e_manejo',
         'profilaxia', 'prognostico', 'complicacoes', 'referencias'
         )
