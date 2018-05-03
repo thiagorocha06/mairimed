b57127760d832b6248d35615ecf3ea7967d026da
Thiago Rocha <thiagorocha06@gmail.com>
2018-05-02 21:40:07 -0300

    Correcao do campo figure do quiz

--- quiz/models.py
+++ quiz/models.py
@@ -521,10 +521,11 @@ class Question(models.Model):
                                      blank=True,
                                      null=True)
 
-    figure = models.ImageField(upload_to='uploads/%Y/%m/%d',
+    figure = models.ImageField(upload_to='img/quiz',
                                blank=True,
                                null=True,
                                verbose_name=_("Figure"))
+
 
     content = models.TextField(max_length=2000,
                                blank=False,
